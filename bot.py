afb9784b25860ad1b61b31f260b2427a6e590b72
zzupart <itszzup23@gmail.com>
2022-09-12 13:54:52 +0000

    base

---
+++ bot.py
@@ -0,0 +1,12 @@
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import config as cfg
+
+async def on_startup(_):
+    print('Bot succesfuly started')
+
+bot = Bot(cfg.TgTOKEN)
+dp = Dispatcher(bot)
+
+executor.start_polling(dp, on_startup = on_startup)

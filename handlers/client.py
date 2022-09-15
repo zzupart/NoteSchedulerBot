10e31beedbe0087709914e465c218c383a0db7a3
zzupart <itszzup23@gmail.com>
2022-09-15 08:44:21 +0000

    added keyboard

--- handlers/client.py
+++ handlers/client.py
@@ -1,20 +1,25 @@
 import re
 from datetime import datetime, timedelta
 from aiogram import types
+from aiogram.types import ReplyKeyboardRemove
 from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from create_bot import bot, dp
 from database import database
+from keyboards import client_kb
 
 class NoteMaker(StatesGroup):
     note = State()
     date = State()
 
 async def command_start(msg: types.message):
-    await bot.send_message(msg.from_user.id, 'Hi!🖐️ I am Note Scheduler - bot, created to help people make notes📜')
+    await bot.send_message(msg.from_user.id, 'Hi!🖐️ I am Note Scheduler - bot, created to help people make notes📜', reply_markup = client_kb.kb_client)
+
+async def command_credits(msg: types.message):
+    bot.send_message(msg.from_user.id, 'Developers of this bot⌨️:\n@osp54\n@zzupart')
 
 async def command_make_note(msg: types.message):
-    await bot.send_message(msg.from_user.id, 'Alright, a new note\nSend message to note')
+    await bot.send_message(msg.from_user.id, 'Alright, a new note\nSend message to note', reply_markup = ReplyKeyboardRemove())
     await NoteMaker.note.set()
 
 async def send_notification(msg: types.message):
@@ -40,10 +45,11 @@ async def send_date(msg: types.message, state: FSMContext):
     try:
         await database.insert_note(msg.from_user.id, datetime.now() + timedelta(seconds=time_in_s), notification)
     except:
-        await bot.send_message(msg.from_user.id, 'Error, please make sure you send correct date')
+        await bot.send_message(msg.from_user.id, 'Error, please make sure you send correct date', reply_markup = client_kb.kb_client)
     else:
-        await bot.send_message(msg.from_user.id, 'New notification created sucessfuly')
+        await bot.send_message(msg.from_user.id, 'New notification created sucessfuly', reply_markup = client_kb.kb_client)
     await state.finish()
+
 async def command_my_notes(msg: types.message):
     rows = await database.execute("""SELECT * FROM notes WHERE user_id = ?""", (msg.from_user.id,), select=True, all=True)
     gen = ""
@@ -53,10 +59,11 @@ async def command_my_notes(msg: types.message):
         await bot.send_message(msg.from_user.id, "Not found your notes.")
     else:
         await bot.send_message(msg.from_user.id, gen)
+
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(send_notification, state = NoteMaker.note)
     dp.register_message_handler(send_date, state = NoteMaker.date)
     dp.register_message_handler(command_start, commands = ['start', 'help'])
-    dp.register_message_handler(command_make_note, commands = ['make_note'])
-    dp.register_message_handler(command_my_notes, commands = ['my_notes'])
-
+    dp.register_message_handler(command_make_note, commands = ['make_note📜'])
+    dp.register_message_handler(command_my_notes, commands = ['my_notes🔔'])
+    dp.register_message_handler(command_my_notes, commands = ['credits💻'])
---
+++ keyboards/client_kb.py
@@ -0,0 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+    
+b1 = KeyboardButton('/make_note📜')
+b2 = KeyboardButton('/my_notes🔔')
+b3 = KeyboardButton('/credits💻')
+
+kb_client = ReplyKeyboardMarkup()
+
+kb_client.row(b1, b2).row(b3)
